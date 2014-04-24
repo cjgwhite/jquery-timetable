02a335761838ae56e6c8def62c8fe4463eb108d8
mfgsscw2 <mfgsscw2@c-s-133-038.csist.manchester.ac.uk>
2014-04-24 14:28:35 +0100

    Adds tt-today class to the tt-day that is the dow :: from PULL #5

--- js/timetable.js
+++ js/timetable.js
@@ -297,12 +297,22 @@
                     return days;
                 },
                 _generateDays: function() {
-                    var eventHandlers = this.events;
-
-                    var days = [];
-                    var dayHeaders = this._generateDayHeaders();
+                    
+                    var eventHandlers = this.events,
+                         days = [],
+                         today = (new Date()).getDay(),
+                         dayHeaders = this._generateDayHeaders();
+                    
                     $.each(dayHeaders, function(index, $day) {
-                        days.push($("<div/>", {"class": "tt-day"}).bind(eventHandlers).append($day));
+//                        days.push($("<div/>", {"class": "tt-day"}).bind(eventHandlers).append($day));
+                        days.push(
+                             $("<div/>", {
+                                 "class": "tt-day" + (today === index ? " tt-today" : ""),
+                                 "role": "row"
+                             })
+                             .on(eventHandlers)
+                             .append($day)
+                         );
                     });
 
                     return days;
